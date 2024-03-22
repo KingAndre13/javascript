ccde9dbc50fb75d4fcb1b037d6c7273eab2f2815
KingAndre13 <andrediogods@gmail.com>
2024-03-22 13:16:06 -0300

    atualização da aula do curso de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 320
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel >= 60) { //Condição simples!
+    console.log(`Misericórdia!!! tá correndo em Interlagos filho do Lewis Hamilton?`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
