a610e54a392e76ea5e57f1bcd62e00554eedf257
KingAndre13 <andrediogods@gmail.com>
2024-04-10 13:38:02 -0300

    Atualização do exercício do curso de JS

--- exs002/int.js
+++ exs002/int.js
@@ -11,15 +11,53 @@ function clicou() {
     var id = rchecked.id
     var msg = document.querySelector('p#msg')
 
-    if (id ==="sxm") {
+    if (id === "sxm" && idade < 12) {
+        msg.innerHTML = `Você é um menino de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/boy.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxm" && idade < 17) {
+        msg.innerHTML = `Você é um menino de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/boyteen.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxm" && idade < 49) {
         msg.innerHTML = `Você é um homem de ${idade} anos!`
         msg.style.textAlign = 'center'
         document.querySelector('div#imagem').style.display = 'block'
-        document.querySelector('div#imagem').style.background = 'blue'
-    } else if (id === 'sxf') {
+        document.querySelector('div#imagem').style.background = 'url(./imagens/man.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxm" && idade >= 50) {
+    msg.innerHTML = `Você é um homem de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/oldman.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxf" && idade < 12) {
+        msg.innerHTML = `Você é uma menina de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/kid.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxf" && idade < 17) {
+        msg.innerHTML = `Você é uma menina de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/teen.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxf" && idade < 49) {
         msg.innerHTML = `Você é uma mulher de ${idade} anos!`
         msg.style.textAlign = 'center'
         document.querySelector('div#imagem').style.display = 'block'
-        document.querySelector('div#imagem').style.background = 'pink'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/woman.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
+    } else if (id === "sxf" && idade >= 50) {
+        msg.innerHTML = `Você é uma mulher de ${idade} anos!`
+        msg.style.textAlign = 'center'
+        document.querySelector('div#imagem').style.display = 'block'
+        document.querySelector('div#imagem').style.background = 'url(./imagens/oldwoman.jpg)'
+        document.querySelector('div#imagem').style.backgroundSize = 'cover'
     }
 }
