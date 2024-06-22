966fb58b065085dddcf597f3c123694990568312
KingAndre13 <andrediogods@gmail.com>
2024-06-22 13:26:13 -0300

    correção do exercício

--- exs005/int.js
+++ exs005/int.js
@@ -6,6 +6,7 @@ let n = []
 
 
 
+
 function adicionar() {
     let number = document.body.querySelector('input#txtn')
     let num = Number(number.value)
@@ -19,27 +20,38 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `o valor ${n[pos]} foi adicionado.`
         s.appendChild(item)
+        res.innerHTML = ''
         }
         }
+        number.value = ''
+        number.focus()
     }
 
 function finalizar() {
     if(n.length == 0) {
        window.alert('Adicione valores antes de finalizar!') 
     } else {
-    n.sort()
     let soma = 0
-    for(let sm = 0; sm < n.length; sm++) {
+    let maior = n[0]
+    let menor = n[0]
+    for(let sm in n) {
         soma += n[sm]
+        if (n[sm] > maior) {
+            maior = n[sm]
+        }
+        if (n[sm] < menor) {
+            menor = n[sm]
+        }
     }
     let div = soma / n.length
     let res = document.body.querySelector('div#res')
     res.innerHTML = `<p>Ao todo, temos ${n.length} números cadastrados.</p>
-    <p>O maior valor informado foi ${n[n.length-1]}
-    <p>O menor valor informado foi ${n[0]}
+    <p>O maior valor informado foi ${maior}
+    <p>O menor valor informado foi ${menor}
     <p>Somando todos os valores, temos ${soma}.
     <p>A média dos valores digitados é ${div}.`
     }
+    
 }
 
 
