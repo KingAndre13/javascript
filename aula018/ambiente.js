b96d4b2ece7dfd376f2ade8434380f60f41f737a
KingAndre13 <andrediogods@gmail.com>
2024-06-28 14:04:29 -0300

    Atualização dos estudos em JS

---
+++ aula018/ambiente.js
@@ -0,0 +1,13 @@
+//const soma = function(v1,v2){return v1+v2}
+
+const soma = (v1, v2) => v1+v2
+console.log(soma(3, 7))
+
+const nome =n=>n
+console.log(nome('Brasil'))
+
+const add =n=>n+=10
+console.log(add(10))
+
+
+
