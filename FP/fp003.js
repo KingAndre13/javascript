69e77036ab7ecac475634d7b973192fc8a5a8b92
KingAndre13 <andrediogods@gmail.com>
2024-07-02 14:03:47 -0300

    Atualização do exercício 6

---
+++ FP/fp003.js
@@ -0,0 +1,9 @@
+/*const imc = (p=0, a=0) => {
+    return p / (a * a)
+}
+
+console.log(`Seu IMC é de ${imc(64, 1.75)}`)*/
+
+const dobrar = n => n*2
+
+console.log(`o dobro desse número é ${dobrar(50)}`)
