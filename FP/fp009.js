2a37a88db4398e17b917d53f18e90d82f2d1c9c7
André Diogo <andrediogods@gmail.com>
2025-04-03 17:58:43 -0300

    UPQ

---
+++ FP/fp009.js
@@ -0,0 +1,12 @@
+a.addEventListener('click', clicarSteveR)
+b.addEventListener('mouseenter', pilotoInjustisado)
+
+function clicarSteveR() {
+    let a = document.getElementById('ca')
+    a.innerText = 'Jesus é a Salvação!'
+}
+
+function pilotoInjustisado() {
+    let b = document.getElementById('pi')
+    b.innerText = 'Helmut Marko é um velho Gagá!'
+}
