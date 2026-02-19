ed3a599ef52abc1cc7d49dfcb302ff0ff8eda7b7
André Diogo <120501460+KingAndre13@users.noreply.github.com>
2026-02-18 22:11:06 -0300

    Aula 7 de JavaScript

---
+++ curson/aulab05/scripts.js
@@ -0,0 +1,24 @@
+var nome = `André`;
+var sobrenome = `Diogo`;
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof `asd`);
+
+console.log(nomeCompleto);
+
+console.log( typeof `5.292929`);
+ 
+var frase = `Esta é uma frase complexa`;
+
+console.log(frase);
+console.log(typeof frase);
+
+//document.write(`Ele disse: "Olá!"`);
+
+//document.write(`Ele disse: 'Olá!'`);
+
+console.log(`Este número: `+nome);
